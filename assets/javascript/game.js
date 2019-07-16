eef0a37d49a727a3301354d423e7008358f15641
Nicole Marty <nmarty310@gmail.com>
2019-07-16 10:48:44 -0500

    added code for how to display an array of _ for a randomly chosen word from the words array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,10 @@ var words = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
 
 // Tell computer to choose a random word from var words
 var word = words[Math.floor(Math.random() * words.length)];
+
+// Create array for randomly chosen word
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+var remainingLetters = word.length;
