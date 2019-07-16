ae4156ae81ecff2117eddcbe0bf7c8e2d8659685
Nicole Marty <nmarty310@gmail.com>
2019-07-16 11:08:01 -0500

    added code to give a condition to the length of the array loop to restrict it to while still letters left to guess and user still has remaining guesses and during this time to update the array whenever the user makes a guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,19 @@ var words = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
 // Tell computer to choose a random word from var words
 var word = words[Math.floor(Math.random() * words.length)];
 
-// Create array for randomly chosen word
+// Create array for randomly chosen word to loop
 var answerArray = [];
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
 var remainingLetters = word.length;
+
+// While loop to make sure keeps looping as long as long as there are more letters to guess and remaining guesses !== 0
+while ((remainingLetters > 0) && (remainingGuesses !== 0)) {
+    // Show progress on word throughout the time user making guesses
+    document.getElementById("answer-array") = innerHTML(answerArray.join(" "));
+
+    // When user presses key...
+
+
+}
