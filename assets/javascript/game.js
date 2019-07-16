e461d96a4a22a5da22b5b5d966c4afba10e2a71b
Nicole Marty <nmarty310@gmail.com>
2019-07-16 10:45:07 -0500

    restarting javascript after figuring out how to represent the _ marks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,5 @@
 // Creates an array that lists out all of the options for computer generated words.
-var wordList = ["cleopatra", "jane austen", "malala", "marie antoinette"];
+var words = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
 
-// Create variables that hold references to the places in the HTML where we want to display things.
-
-// Score variables
-var wins = 0;
-var guessesRemaining = 10;
-
-// This function is run whenever the user presses a key.
-document.onkey = function(event) {
-    // For loop
-    for (var i = 0;)
-
-}
-
-// determines which key was pressed
-var userGuess = event.key;
-
-// If the user presses a correct letter...
-if ((userGuess === "c") || (userGuess === "l") || (userGuess === "e") || (userGuess === "o") || (userGuess === "p") ||
-    (userGuess === "a") || (userGuess === "t") || (userGuess === "r") || (userGuess === "a")) {
-    // then run this code...
-
-}
+// Tell computer to choose a random word from var words
+var word = words[Math.floor(Math.random() * words.length)];
