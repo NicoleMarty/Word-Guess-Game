8df94174a0e6fca865452f670110d9f025ea927c
Nicole Marty <nmarty310@gmail.com>
2019-07-16 16:15:26 -0500

    not working so created additional js file to walk through step by step starting with the variables to see what I'm missing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,39 @@
+// Variables
+
 // Creates an array that lists out all of the options for computer generated words.
 var words = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
+// When user presses key...
+document.onkeyup = function(event) {
+    // Tell computer to choose a random word from var words
+    var word = words[Math.floor(Math.random() * words.length)];
 
-// Tell computer to choose a random word from var words
-var word = words[Math.floor(Math.random() * words.length)];
+    // Create array to be filled by random word on a loop
+    var answerArray = [];
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+    }
+    var remainingLetters = word.length;
 
-// Create array for randomly chosen word to loop
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
-var remainingLetters = word.length;
+    // While loop to make sure keeps looping as long as long as there are more letters to guess and remaining guesses !== 0
+    while (remainingLetters > 0) {
+        // Show progress on word throughout the time user making guesses
+        document.getElementById("answer-array").innerHTML(answerArray.join(" "));
 
-// While loop to make sure keeps looping as long as long as there are more letters to guess and remaining guesses !== 0
-while ((remainingLetters > 0) && (remainingGuesses !== 0)) {
-    // Show progress on word throughout the time user making guesses
-    document.getElementById("answer-array") = innerHTML(answerArray.join(" "));
+        // Create another loop to run each time the user guesses a letter to check if the choice matches any of the _ in word
+        for (var k = 0; k < word.length; k++) {
+            if (word[k] === choice) {
+                answerArray[k] = choice;
+                remainingLetters--;
 
-    // When user presses key...
-    //create another loop to run each time the user guesses a letter to check if the choice matches any of the _ in word
-    for (var k = 0; k < word.length; k++) {
-        if (word[k] === choice) {
-            answerArray[k] = choice;
-            remainingLetters--;
-            // decrease remainingGuesses
-            // add choice to letterGuessesd
+                // decrease remainingGuesses
+                // add choice to letterGuessesd
+            }
         }
     }
 }
+
 
 // if remaining letters === 0
 
 
 // if remaining guesses === 0
-
-
-
-}
---
+++ assets/javascript/game2.js
@@ -0,0 +1,10 @@
+// Set variables
+var word;
+var guessesAllowed;
+var guessesCorrect;
+var guessesWrong;
+
+var wordArray = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
+var wordChoiceElement = document.getElementById('wordChoice');
+var letterCountElement = document.getElementById("letterCount");
+var userGuessElement = document.getElementById("userGuess");
