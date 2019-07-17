30af3c36badf5f4ad251e8e8cfeb00f6ace0ad3a
Nicole Marty <nmarty310@gmail.com>
2019-07-17 00:17:47 -0500

    closer with the press any key to start game. now displays key pressed, but need it to display _ _ _ _ _ ... representing the chosen name

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -2,9 +2,7 @@
 var directionsText = document.getElementById("directions-text");
 
 var computerOptions = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
-var wordChoiceElement = document.getElementById("word-choice");
-var computerArray;
-var optionIndex = 0;
+
 
 var userGuess;
 var userGuessElement = document.getElementById("user-guess");
@@ -20,14 +18,7 @@ var wins = 0;
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 
-// Start the game
-// Function is run whenever user presses a key
+var wordChoiceElement = document.getElementById("word-choice");
 document.onkeyup = function(event) {
-    // Computer chooses an option from computerOptions
-    function startGame()
-    var computerArray = [];
-    for (var i = 0; i < computerOptions.length; i++) {
-        computerArray[i] = "_";
-    }
-    var remainingLetters = computerOptions.length;
-}
+    wordChoiceElement.textContent = event.key;
+};
