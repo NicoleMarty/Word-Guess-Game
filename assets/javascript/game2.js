7c003457e566951480f3706b58928769b43d22cb
Nicole Marty <nmarty310@gmail.com>
2019-07-16 23:03:16 -0500

    moved script tag to end of body

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -2,9 +2,9 @@
 var directionsText = document.getElementById("directions-text");
 
 var computerOptions = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
-var computerChoice;
 var wordChoiceElement = document.getElementById("word-choice");
 var computerArray;
+var optionIndex = 0;
 
 var userGuess;
 var userGuessElement = document.getElementById("user-guess");
@@ -21,3 +21,13 @@ var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 
 // Start the game
+// Function is run whenever user presses a key
+document.onkeyup = function(event) {
+    // Computer chooses an option from computerOptions
+    function startGame()
+    var computerArray = [];
+    for (var i = 0; i < computerOptions.length; i++) {
+        computerArray[i] = "_";
+    }
+    var remainingLetters = computerOptions.length;
+}
