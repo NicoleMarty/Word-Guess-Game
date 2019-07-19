6797020bcc76bbe8af8900f355ff73db553ce4db
Nicole Marty <nmarty310@gmail.com>
2019-07-19 14:37:02 -0500

    javascript file updated with changes made in class on thursday. still not working...blah. coming back to this later. needs to not be worked on for a day.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,36 @@
-// Variables
 var userGuess;
 var remainingLetters;
 
-//Each element of the array is marked by an index. Indexes always start with 0.
-//Can be made up of other arrays and objects.
 var nameArray = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
-// ex: cleopatra=index[0]
-var nameArrayValues = ["_"]
-
 var allowedGuesses = 12;
-var wins = 1;
+var lettersGuessed = [];
+var indexOfCurrentName;
+var wins = 0;
+var losses = 0;
+var currentName = [];
+var gameEnd = false;
+var nameBuild = [];
 
 var currentNameElement = document.getElementById("current-name");
 var userGuessElement = document.getElementById("letters-guessed");
 var winsElement = document.getElementById("wins-count");
 var allowedGuessesElement = document.getElementById("remaining-guesses");
-
-// used to tell when user wins
 var letterCountElement = document.getElementById("remaining-letters");
 
-// for loop 
-// variable declaration or counter (iterator)
-//loop condition
-//iteration (addition)
-document.onkeyup = function(event) {
-    var userGuess = event.key;
-    userGuessElement.innerHTML = userGuess;
-}
 var nameArray = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
-document.onkeyup = function(event) {
-    var userGuess = event.key;
-    var currentName = nameArray[Math.floor(Math.random() * nameArray.length)];
-    for (var i = 1; i < currentName.length; i++) {
-        console.log(currentName[i]);
-        if (currentName[i] === "cleopatra"); {
-            currentNameElement.innerHTML = ["_", "_", "_", "_", "_", "_", "_", "_", "_"];
-        }
-        if (currentName[i] === "malala"); {
-            currentNameElement.innerHTML = ["_", "_", "_", "_", "_", "_"];
+var name = Math.floor(Math.random() * nameArray.length);
+var nameChoice = nameArray[name];
+for (var i = 0; i < nameChoice.length; i++) {
 
-        }
-        if (currentName[i] === "marie antoinette"); {
-            currentNameElement.innerHTML = ["marie" + "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
-        }
+    if (nameChoice === "cleopatra"); {
+        currentNameElement.innerhtml = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
     }
 
+    if (nameChoice === "malala"); {
+        currentNameElement.innerHTML = ["_", "_", "_", "_", "_", "_"];
+    }
+
+    if (nameChoice === "marie antoinette"); {
+        currentNameElement.innerHTML = ["marie" + "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
+    }
 }
