722f8ff05147948cbb4ff78279417a74cdbaacba
Nicole Marty <nmarty310@gmail.com>
2019-08-14 11:13:22 -0500

    rebuilding now that I have time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,38 @@
 console.log("page loaded");
 
-var userGuess;
-var remainingLetters;
-
-var nameArray = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
-var allowedGuesses = 12;
-var lettersGuessed = [];
-var indexOfCurrentName;
+// Variables
+var currentChoice = ""; // "current name"
+var nameBuild = []; // number of letters in current name
+var numGuesses = 0; // number of letters guessed
+var lettersGuessed = []; // Store the guessed letters;
+var indexOfCurrentName; // Store the "_" and to be used to replace the word answer
 var wins = 0;
 var losses = 0;
-var currentName = [];
-var gameEnd = false;
-var nameBuild = [];
 
-var currentNameElement = document.getElementById("current-name");
-var userGuessElement = document.getElementById("letters-guessed");
-var winsElement = document.getElementById("wins-count");
-var allowedGuessesElement = document.getElementById("remaining-guesses");
-var letterCountElement = document.getElementById("remaining-letters");
+var gameEnd = false;
+var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"];
+
+var currentNameElement = document.getElementById("current-choice")
+var userGuessElement = document.getElementById("letters-guessed")
+var winsElement = document.getElementById("wins-count")
+var allowedGuessesElement = document.getElementById("guesses-num")
+var letterCountElement = document.getElementById("letter-num")
 
 var nameArray = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
 var name = Math.floor(Math.random() * nameArray.length);
-var nameChoice = nameArray[name];
-console.log(nameChoice);
+var currentChoice = nameArray[name];
+console.log(currentChoice);
 
-// for loop to change the words into underscores
-if (nameChoice === "cleopatra") {
-    currentNameElement.textContent = ["_ _ _ _ _ _ _ _ _"];
-} else if (nameChoice === "malala") {
-    currentNameElement.innerHTML = ["_ _ _ _ _ _"];
-} else if (nameChoice === "marie antoinette") {
-    currentNameElement.innerHTML = ["marie _ _ _ _ _ _ _ _ _ _"];
-} else if (nameChoice === "harriet tubman") {
-    currentNameElement.innerHTML = ["_ _ _ _ _ _ t _ _ _ _ _ _"];
+// Functions
+
+
+// Main Process to choose name
+if (currentChoice === "cleopatra") {
+    currentNameElement.innerHTML = ["_ _ _ _ _ _ _ _ _"]
+} else if (currentChoice === "malala") {
+    currentNameElement.innerHTML = ["_ _ _ _ _ _"]
+} else if (currentChoice === "marie antoinette") {
+    currentNameElement.innerHTML = ["marie _ _ _ _ _ _ _ _ _ _"]
+} else if (currentChoice === "harriet tubman") {
+    currentNameElement.innerHTML = ["_ _ _ _ _ _ t _ _ _ _ _ _"]
 }
