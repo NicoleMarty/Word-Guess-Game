e765f2eb64f294ea915b919bdc8509f7833ffd86
Nicole Marty <nmarty310@gmail.com>
2019-08-14 12:00:36 -0500

    working on getting number of letters in a string and their index defined

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 console.log("page loaded");
 
 // Variables
-var currentChoice = ""; // "current name"
+var currentChoice; // "current name"
 var nameBuild = []; // number of letters in current name
 var numGuesses = 0; // number of letters guessed
 var lettersGuessed = []; // Store the guessed letters;
-var indexOfCurrentName; // Store the "_" and to be used to replace the word answer
+var indexOfCurrentName = ["_ _ _ _ _ _ _ _ _", "_ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _ _ _"];
 var wins = 0;
 var losses = 0;
-
+var i = "_"
 var gameEnd = false;
 var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"];
 
@@ -24,15 +24,17 @@ var currentChoice = nameArray[name];
 console.log(currentChoice);
 
 // Functions
-
+//for (var i = 0; i < nameBuild[i].length;)
 
 // Main Process to choose name
 if (currentChoice === "cleopatra") {
-    currentNameElement.innerHTML = ["_ _ _ _ _ _ _ _ _"]
+    currentNameElement.innerHTML = ["_ _ _ _ _ _ _ _ _"];
+
 } else if (currentChoice === "malala") {
-    currentNameElement.innerHTML = ["_ _ _ _ _ _"]
+    currentNameElement.innerHTML = ["_ _ _ _ _ _"];
 } else if (currentChoice === "marie antoinette") {
-    currentNameElement.innerHTML = ["marie _ _ _ _ _ _ _ _ _ _"]
+    currentNameElement.innerHTML = ["_ _ _ _ _ _ _ _ _ _ _ _ _ _ _"];
 } else if (currentChoice === "harriet tubman") {
-    currentNameElement.innerHTML = ["_ _ _ _ _ _ t _ _ _ _ _ _"]
+    currentNameElement.innerHTML = ["_ _ _ _ _ _ _ _ _ _ _ _ _"];
+    console.log(letterCountElement)
 }
