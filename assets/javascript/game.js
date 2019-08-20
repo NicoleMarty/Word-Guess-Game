e47fd52cf228ff8e59a8b23af1898020c16dd262
Nicole Marty <nmarty310@gmail.com>
2019-08-20 17:52:05 -0500

    working on getting computer to recognize the letters in the name as index numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,15 @@ var currentChoice; // "current name" selected by computer
 var nameBuild = []; // number of letters in "current name"
 var numGuesses = 0; // number of letters guessed
 var lettersGuessed = []; // Store the guessed letters;
-var indexOfCurrentName = ["_ _ _ _ _ _ _ _ _", "_ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _", "_ _ _ _ _ _ _ _ _ _ _ _ _"];
+var nameObject = {
+    cleopatra: "_ _ _ _ _ _ _ _ _",
+    malala: "_ _ _ _ _ _",
+    marieantoinette: "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _",
+    harriettubman: "_ _ _ _ _ _ _ _ _ _ _ _ _"
+};
+var nameIndex = 0;
+var userGuess;
+
 var wins = 0;
 var losses = 0;
 var gameEnd = false;
@@ -15,32 +23,28 @@ var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "
 var currentNameElement = document.getElementById("current-choice")
 var userGuessElement = document.getElementById("user-guess")
 var winsElement = document.getElementById("wins-count")
-var allowedGuessesElement = document.getElementById("guesses-num")
+var allowedGuessesElement = document.getElementById("letters-guessed")
 var letterCountElement = document.getElementById("letter-num")
 
-/*document.getElementById("user-guess").on,("keyup", function();
-function startGameFunction() {
-    document.getElementById("letters-guessed").textContent
-}*/
+
 
 var nameArray = ["cleopatra", "malala", "marie antoinette", "harriet tubman"];
 var name = Math.floor(Math.random() * nameArray.length);
 var currentChoice = nameArray[name];
 console.log(currentChoice);
-
-// Main Process to choose name
 if (currentChoice === "cleopatra") {
     currentNameElement.innerHTML = ["_ _ _ _ _ _ _ _ _"];
+    console.log(nameObject.cleopatra)
+    nameBuild = ["c", "l", "e", "o", "p", "a", "t", "r", "a"];
+    console.log(nameBuild[0])
+
 } else if (currentChoice === "malala") {
     currentNameElement.innerHTML = ["_ _ _ _ _ _"];
+    console.log(nameObject.malala)
 } else if (currentChoice === "marie antoinette") {
     currentNameElement.innerHTML = ["_ _ _ _ _ _ _ _ _ _ _ _ _ _ _"];
+    console.log(nameObject.marieantoinette)
 } else if (currentChoice === "harriet tubman") {
     currentNameElement.innerHTML = ["_ _ _ _ _ _ _ _ _ _ _ _ _"];
-    console.log(letterCountElement)
+    console.log(nameObject.harriettubman)
 }
-
-function myFunction() {
-    var x = document.getElementById("user-guess").value;
-    document.getElementById("letters-guessed").innerHTML = x;
-};
